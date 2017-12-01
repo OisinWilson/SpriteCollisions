6ff5e358c3a3b6d12a0578947d0a8913fcfab548
Oisin <oisinwilson@gmail.com>
2017-12-01 18:30:02 +0000

    Update - Most collision working

--- MuddyGames-gameplay-programming-i-lab-03-a-3a5f9c674059/MuddyGames-gameplay-programming-i-lab-03-a-3a5f9c674059/2DCollision/main.cpp
+++ MuddyGames-gameplay-programming-i-lab-03-a-3a5f9c674059/MuddyGames-gameplay-programming-i-lab-03-a-3a5f9c674059/2DCollision/main.cpp
@@ -52,6 +52,9 @@ int main()
 	circle_mouse.r = 42;
 
 	c2Ray ray_mouse;
+	ray_mouse.p = c2V(aabb_mouse.min.x, aabb_mouse.min.y);
+	ray_mouse.d = c2Norm(c2V(1, 1));
+	ray_mouse.t = 125;
 
 	// Setup Players Default Animated Sprite
 	AnimatedSprite animated_sprite(sprite_sheet);
@@ -63,27 +66,36 @@ int main()
 	animated_sprite.addFrame(sf::IntRect(428, 3, 84, 84));
 
 	// Setup Player
+	c2Manifold manifold;
+	c2Raycast cast;
+
 	c2AABB aabb_player;
 	aabb_player.min = c2V(animated_sprite.getPosition().x, animated_sprite.getPosition().y);
 	aabb_player.max = c2V(animated_sprite.getGlobalBounds().width / animated_sprite.getFrames().size(),
 		animated_sprite.getGlobalBounds().height / animated_sprite.getFrames().size());
 
 	c2Capsule capsule_player;
-
+	capsule_player.a = c2V(500, 150);
+	capsule_player.b = c2V(600, 150);
+	capsule_player.r = 42;
 
 	c2Poly polygon_player;
 	polygon_player.count = 4;
-	polygon_player.verts[0] = c2V(100,0);
-	polygon_player.verts[1] = c2V(100,85);
-	polygon_player.verts[2] = c2V(185,85);
-	polygon_player.verts[3] = c2V(185,0);
+	polygon_player.verts[0] = c2V(200,100);
+	polygon_player.verts[1] = c2V(200,185);
+	polygon_player.verts[2] = c2V(285,185);
+	polygon_player.verts[3] = c2V(285,100);
 	c2MakePoly(&polygon_player);
-	c2Manifold manifold;
-
+	
 
 	c2Circle circle_player;
-
+	circle_player.r = { 42 };
+	circle_player.p = { 120,500 };
+	
 	c2Ray ray_player;
+	ray_player.p = c2V(500, 500);
+	ray_player.d = c2Norm(c2V(2, -1));
+	ray_player.t = 200;
 
 
 	// Setup the Player
@@ -104,6 +116,7 @@ int main()
 		aabb_mouse.max = c2V(mouse.getPosition().x + mouse.getGlobalBounds().width, mouse.getPosition().y + mouse.getGlobalBounds().height);
 
 		circle_mouse.p = { aabb_mouse.min.x + 42, aabb_mouse.min.y + 42 };
+		ray_mouse.p = { aabb_mouse.min.x, aabb_mouse.min.y };
 
 
 		// Process events
@@ -169,34 +182,50 @@ int main()
 			result = c2AABBtoAABB(aabb_mouse, aabb_player);
 			cout << ((result != 0) ? ("Collision AABB - AABB") : "") << endl;
 
-			//result = c2AABBtoCapsule(aabb_mouse, capsule_player);
-			//cout << ((result != 0) ? ("Collision AABB - Capsule") : "") << endl;
+			result = c2AABBtoCapsule(aabb_mouse, capsule_player);
+			cout << ((result != 0) ? ("Collision AABB - Capsule") : "") << endl;
 
 			c2AABBtoPolyManifold(aabb_mouse, &polygon_player, NULL, &manifold);
 			cout << ((manifold.count != 0) ? ("Collision AABB - Poly") : "") << endl;
 
-			//ray
+			result = c2RaytoAABB(ray_player, aabb_mouse, &cast);
+			cout << ((result != 0) ? ("Collision AABB - Ray") : "") << endl;
+
+			result = c2CircletoAABB(circle_player, aabb_mouse);
+			cout << ((result != 0) ? ("Collision AABB - Circle") : "") << endl;
 		}
 		else if (mouse_changer[1]) // Circle
 		{
 			result = c2CircletoAABB(circle_mouse, aabb_player);
 			cout << ((result != 0) ? ("Collision Circle - AABB") : "") << endl;
 
-			//result = c2CircletoCircle(circle_mouse, circle_player);
-			//cout << ((result != 0) ? ("Collision Circle - Circle") : "") << endl;
+			result = c2CircletoCircle(circle_mouse, circle_player);
+			cout << ((result != 0) ? ("Collision Circle - Circle") : "") << endl;
 
-			//result = c2CircletoCapsule(circle_mouse, capsule_player);
-			//cout << ((result != 0) ? ("Collision Circle - Capsule") : "") << endl;
+			result = c2CircletoCapsule(circle_mouse, capsule_player);
+			cout << ((result != 0) ? ("Collision Circle - Capsule") : "") << endl;
 
-			//result = c2CircletoPoly(circle_mouse, polygon_player, transformation);
-			//cout << ((result != 0) ? ("Collision Circle - Poly") : "") << endl;
+			c2CircletoPolyManifold(circle_mouse, &polygon_player, NULL, &manifold);
+			cout << ((manifold.count != 0) ? ("Collision Circle - Poly") : "") << endl;
 
-			//ray
+			//issues
+			result = c2RaytoCircle(ray_player, circle_mouse, &cast);
+			cout << ((result != 0) ? ("Collision Circle - Ray") : "") << endl;
 
 		}
 		else if (mouse_changer[2]) // Ray
 		{
-			
+			result = c2RaytoAABB(ray_mouse, aabb_player, &cast);
+			cout << ((result != 0) ? ("Collision Ray - AABB") : "") << endl;
+
+			result = c2RaytoCircle(ray_mouse, circle_player, &cast);
+			cout << ((result != 0) ? ("Collision Ray - Circle") : "") << endl;
+
+			result = c2RaytoCapsule(ray_mouse, capsule_player, &cast);
+			cout << ((result != 0) ? ("Collision Ray - Capsule") : "") << endl;
+
+			result = c2RaytoPoly(ray_mouse,&polygon_player,NULL,&cast);
+			cout << ((result != 0) ? ("Collision Ray - Poly") : "") << endl;
 		}
 		
 
@@ -217,8 +246,16 @@ int main()
 		// Draw the Players Current Animated Sprite
 		window.draw(player.getAnimatedSprite());
 		window.draw(mouse);
-		g.draw(aabb_mouse, sf::Color::Blue);
+
+		//Remove later
+		g.draw(aabb_mouse, sf::Color::Cyan);
+		g.draw(circle_mouse, sf::Color::Cyan);
+		g.draw(ray_mouse, sf::Color::Cyan);
+
 		g.draw(polygon_player, sf::Color::Blue);
+		g.draw(circle_player, sf::Color::Blue);
+		g.draw(capsule_player, sf::Color::Blue);
+		g.draw(ray_player, sf::Color::Blue);
 
 		// Update the window
 		window.display();
